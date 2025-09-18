85a1c1b82dc5b5ec8bc74607fe35b2d7c955c1ca
maxitabakman <49928297@est.ort.edu.ar>
2025-09-18 12:08:33 -0300

    que mierda de tp

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ import {
   seleccionarProducto,
   seleccionarSabores,
 } from "./dataEntry.js";
+import fs from "fs"
 
 // Cargar sabores y productos
 // COMPLETEN USTEDES
-let sabores;
-let productos;
+let sabores=JSON.parse(fs.readFileSync("data/sabores.json", "utf-8"));
+
+let productos=JSON.parse(fs.readFileSync("data/productos.json", "utf-8"));;
 
 // Ingresar cliente
 let cliente = ingresarCliente();
