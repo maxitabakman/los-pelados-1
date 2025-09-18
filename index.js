2fd9d443a40cf37a8a1cbfb603081066eb482e47
maxitabakman <49928297@est.ort.edu.ar>
2025-09-18 11:18:46 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+import {
+  seleccionarCantidadGustos,
+  ingresarCliente,
+  seleccionarProducto,
+  seleccionarSabores,
+} from "./dataEntry.js";
+
+// Cargar sabores y productos
+// COMPLETEN USTEDES
+let sabores;
+let productos;
+
+// Ingresar cliente
+let cliente = ingresarCliente();
+// Elegir producto
+let producto = seleccionarProducto(productos);
+// Elegir cantidad de gustos
+let gustos = seleccionarCantidadGustos(producto.maxGustos);
+// Elegir sabores
+let saboresElegidos = seleccionarSabores(sabores, gustos);
+
+// Guardar pedido
+// COMPLETEN USTEDES
